2cdd0fabe493fe56ad5dbd34c6c61b1a0c226df2
Rusty <rusty@hotperformancehorses.com>
2023-07-06 13:19:45 -0500

    js code startover, questions added

--- script.js
+++ script.js
@@ -1,143 +1,53 @@
-// Declare global variables 
-const startBtn = document.getElementById("startBtn");
-let time = 75;
-let timeRemaining = true;
-let timeStart = false;
-const countdownTimer = document.getElementById("countdownTimer");
-const homeContainer = document.getElementById("homeContainer");
-const quizContainer = document.getElementById("quizContainer");
-const questionHeading = document.getElementById("questionHeading");
-const answerChoiceA = document.getElementById("answerChoiceA");
-const answerChoiceB = document.getElementById("answerChoiceB");
-const answerChoiceC = document.getElementById("answerChoiceC");
-const answerChoiceD = document.getElementById("answerChoiceD");
-const correctAnswer = document.getElementById("correctAnswer");
-const highScores = [];
-let output = "";
-let score = 0;
-let questionIndex = 0;
+// Declare variables 
+var startBtn = document.getElementById("startBtn");
+var time = 75;
+var time_remaining = true;
+var time_start= false;
+var countdownTimer = document.getElementById("countdownTimer");
+var homeContainer =  document.getElementById("homeContainer");
+var quizContainer = document.getElementById("quizContainer");
+var questionHeading = document.getElementById("questionHeading");
+var answerChoiceA = document.getElementById("answerChoiceA");
+var answerChoiceB = document.getElementById("answerChoiceB");
+var answerChoiceC = document.getElementById("answerChoiceC");
+var answerChoiceD = document.getElementById("answerChoiceD");
+var correctAnswer = document.getElementById("correctAnswer");    
+var high_scores= [];
+var output="";
+// Set score = 0 a
+var score = 0;
+// question
+let i = 0;
 
-// QUESTIONS ARRAY:
-const questionsArray = [
-  {
-    question: "Question: What is the HTML tag under which you can write the JavaScript code?",
-    imageSrc: "",
-    answerChoice: ["A) <javascript>", "B) <scripted>", "C) <script>", "D) <js>"],
-    correctAnswer: 2
-  },
-  // ... rest of the questions ...
-];
+// questions:
 
-// Countdown timer function
-const countdownTimerInterval = setInterval(setCountdownTimer, 1000);
-
-function setCountdownTimer() {
-  if (timeStart) {
-    time--;
-  }
-  if (time <= 0) {
-    endQuiz();
-    time = 0;
-  }
-  document.getElementById("timer").innerHTML = time;
-}
-
-// Start event listener
-startBtn.addEventListener("click", function() {
-  quizContainer.style.display = "block";
-  homeContainer.style.display = "none";
-  countdownTimer.style.display = "block";
-  document.getElementById("score_keeper").style.display = "block";
-  document.getElementById("score").innerHTML = score;
-  setCountdownTimer();
-  setQuizQuestions();
-  timeStart = true;
-});
-
-// Set quiz questions
-function setQuizQuestions() {
-  questionHeading.textContent = questionsArray[questionIndex].question;
-  answerChoiceA.textContent = questionsArray[questionIndex].answerChoice[0];
-  answerChoiceB.textContent = questionsArray[questionIndex].answerChoice[1];
-  answerChoiceC.textContent = questionsArray[questionIndex].answerChoice[2];
-  answerChoiceD.textContent = questionsArray[questionIndex].answerChoice[3];
-}
-
-// Event listeners for answer choices
-answerChoiceA.addEventListener('click', function(event) {
-  event.stopPropagation();
-  const correctAnswerIndex = questionsArray[questionIndex].correctAnswer;
-  if (0 === correctAnswerIndex) {
-    document.getElementById("AnswerResponse").innerHTML = "Correct! Nailed it!";
-    setTimeout(function() {
-      document.getElementById("AnswerResponse").innerHTML = "";
-    }, 1000);
-    score++;
-    document.getElementById("score").innerHTML = score;
-  } else {
-    timeRemaining -= 5;
-    document.getElementById("AnswerResponse").innerHTML = "Incorrect! Better luck in the next one!";
-    setTimeout(function() {
-      document.getElementById("AnswerResponse").innerHTML = "";
-    }, 1000);
-  }
-  if (questionIndex >= questionsArray.length - 1) {
-    endQuiz();
-  } else {
-    questionIndex++;
-    setQuizQuestions();
-  }
-});
-
-// ... repeat the above event listeners for answerChoiceB, answerChoiceC, and answerChoiceD ...
-
-// End quiz function
-function endQuiz() {
-  document.getElementById("game_over").style.display = "block";
-  document.getElementById("quizContainer").style.display = "none";
-  document.getElementById("countdownTimer").style.display = "none";
-  document.getElementById("score_keeper").style.display = "none";
-  document.getElementById("AnswerResponse").innerHTML = "";
-  document.getElementById("end_score").innerHTML = score;
-}
-
-// Submit score and initials
-function submitScore() {
-  highScores.push(document.getElementById("initials").value + " " + score);
-  viewHighScores();
-}
-
-// View high scores
-function viewHighScores() {
-  document.getElementById("quizContainer").style.display = "none";
-  document.getElementById("game_over").style.display = "none";
-  document.getElementById("high_scores_page").style.display = "block";
-  
-  output = "";
-  for (let k = 0; k < highScores.length; k++) {
-    output += " " + highScores[k];
-  }
-  document.getElementById("high_scores").innerHTML = output;
-  clearUp();
-}
-
-// Go back to home page
-function goHome() {	
-  document.getElementById("high_scores_page").style.display = "none";
-  document.getElementById("homeContainer").style.display = "block";
-  clearUp();
-}
-
-// Clear high scores
-function clearHighScores() {
-  highScores.splice(0, highScores.length);
-}
-
-// Clear up variables
-function clearUp() {
-  time = 75;
-  timeRemaining = true;
-  timeStart = false;
-  questionIndex = 0;
-  score = 0;
-}
+var questionsArray = [
+{
+question: "Question: What is the HTML tag under which you can write the JavaScript code?",
+imageSrc: "",
+answerChoice: ["A) <javascript>", "B) <scripted>", "C) <script>", "D) <js>"],
+correctAnswer: 2
+}, 
+{
+question: "Question: What are variables used for in JavaScript Programs?",
+imageSrc: "",
+answerChoice: ["A) Storing numbers, dates, or other values", "B) Varying randomly", "D) Causing high-school algebra flashbacks", "D) None of the above"],
+correctAnswer: 0
+},
+{
+question: "Question: Which method adds a new item to the end of an array and returns the new length?",
+imageSrc: "",
+answerChoice: ["A) shift()", "B) return() ", "C) push() ", "D) pop()"],
+correctAnswer: 2
+}, 
+{
+question: "Question: Which of the following can't be done with client-side JavaScript?",
+imageSrc: "",
+answerChoice: ["A) Sending a form's contents by email", "B) Validating a form", "C) Storing the form's contents to a database file on the server", "D) None of the above"],
+correctAnswer: 2
+},
+{
+question: "Question: Which of the following are capabilities of functions in JavaScript?",
+answerChoice: ["A) Return a value", "B) Accept parameters", "C) Accept parameters and Return a value", "D) All of the above"],
+correctAnswer: 1
+}];
