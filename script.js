b01f719bb5d3f98fb920a231b447bfeb34e177d4
Rusty <rusty@hotperformancehorses.com>
2023-07-06 13:30:45 -0500

    more js edits to fix deployment issues

--- script.js
+++ script.js
@@ -1,60 +1,62 @@
-// Declare variables 
-var startBtn = document.getElementById("startBtn");
-var time = 75;
-var time_remaining = true;
-var time_start= false;
-var countdownTimer = document.getElementById("countdownTimer");
-var homeContainer =  document.getElementById("homeContainer");
-var quizContainer = document.getElementById("quizContainer");
-var questionHeading = document.getElementById("questionHeading");
-var answerChoiceA = document.getElementById("answerChoiceA");
-var answerChoiceB = document.getElementById("answerChoiceB");
-var answerChoiceC = document.getElementById("answerChoiceC");
-var answerChoiceD = document.getElementById("answerChoiceD");
-var correctAnswer = document.getElementById("correctAnswer");    
-var high_scores= [];
-var output="";
-// Set score = 0 a
-var score = 0;
-// question
-let i = 0;
+// When user clicks the start button, then a timer starts and user is presented with a question
 
-// questions:
+    // Declare global variables 
+    var startBtn = document.getElementById("startBtn");
+    var time = 75;
+    var time_remaining = true;
+    var time_start= false;
+    var countdownTimer = document.getElementById("countdownTimer");
+    var homeContainer =  document.getElementById("homeContainer");
+    var quizContainer = document.getElementById("quizContainer");
+    var questionHeading = document.getElementById("questionHeading");
+    var answerChoiceA = document.getElementById("answerChoiceA");
+    var answerChoiceB = document.getElementById("answerChoiceB");
+    var answerChoiceC = document.getElementById("answerChoiceC");
+    var answerChoiceD = document.getElementById("answerChoiceD");
+    var correctAnswer = document.getElementById("correctAnswer");    
+    var high_scores= [];
+    var output="";
+    // Set score = 0 at the start of the game 
+    var score = 0;
+    // question index
+    let i = 0;
+
+// QUESTIONS ARRAY:
 
 var questionsArray = [
 {
-question: "Question: What is the HTML tag under which you can write the JavaScript code?",
-imageSrc: "",
-answerChoice: ["A) <javascript>", "B) <scripted>", "C) <script>", "D) <js>"],
-correctAnswer: 2
+    question: "Question: What is the HTML tag under which you can write the JavaScript code?",
+    imageSrc: "",
+    answerChoice: ["A) <javascript>", "B) <scripted>", "C) <script>", "D) <js>"],
+    correctAnswer: 2
 }, 
 {
-question: "Question: What are variables used for in JavaScript Programs?",
-imageSrc: "",
-answerChoice: ["A) Storing numbers, dates, or other values", "B) Varying randomly", "D) Causing high-school algebra flashbacks", "D) None of the above"],
-correctAnswer: 0
+    question: "Question: What are variables used for in JavaScript Programs?",
+    imageSrc: "",
+    answerChoice: ["A) Storing numbers, dates, or other values", "B) Varying randomly", "D) Causing high-school algebra flashbacks", "D) None of the above"],
+    correctAnswer: 0
 },
 {
-question: "Question: Which method adds a new item to the end of an array and returns the new length?",
-imageSrc: "",
-answerChoice: ["A) shift()", "B) return() ", "C) push() ", "D) pop()"],
-correctAnswer: 2
+    question: "Question: Which method adds a new item to the end of an array and returns the new length?",
+    imageSrc: "",
+    answerChoice: ["A) shift()", "B) return() ", "C) push() ", "D) pop()"],
+    correctAnswer: 2
 }, 
 {
-question: "Question: Which of the following can't be done with client-side JavaScript?",
-imageSrc: "",
-answerChoice: ["A) Sending a form's contents by email", "B) Validating a form", "C) Storing the form's contents to a database file on the server", "D) None of the above"],
-correctAnswer: 2
+    question: "Question: Which of the following can't be done with client-side JavaScript?",
+    imageSrc: "",
+    answerChoice: ["A) Sending a form's contents by email", "B) Validating a form", "C) Storing the form's contents to a database file on the server", "D) None of the above"],
+    correctAnswer: 2
 },
 {
-question: "Question: Which of the following are capabilities of functions in JavaScript?",
-answerChoice: ["A) Return a value", "B) Accept parameters", "C) Accept parameters and Return a value", "D) All of the above"],
-correctAnswer: 1
+    question: "Question: Which of the following are capabilities of functions in JavaScript?",
+    answerChoice: ["A) Return a value", "B) Accept parameters", "C) Accept parameters and Return a value", "D) All of the above"],
+    correctAnswer: 1
 }];
 
-//countdown timer/variables.
+//COUNTDOWN TIMER FUNCTION: set countdown timer and interval. Set time-related valiables.
 
-//change the seconds variable.
+//change the seconds variable every second.
 var countdownTimerInterval = setInterval(setCountdownTimer, 1000);
 
 //function that changes the time var
@@ -65,12 +67,12 @@ function setCountdownTimer() {
         end_quiz();
         time = 0;    
         // clearInterval(countdownTimerInterval);
-        // stop quiz
+        //alert user and stop quiz
         }
         document.getElementById("timer").innerHTML = time;
     }
 
-// When user clicks Start button, start the countdown timer and quiz questions. Add an event listener to each button.
+// START EVENT LISTENER: When user clicks Start button, start the countdown timer and quiz questions. Add an event listener to each button.
 startBtn.addEventListener("click", function() {
     quizContainer.style.display = "block";
     homeContainer.style.display ="none";
@@ -82,7 +84,7 @@ startBtn.addEventListener("click", function() {
     time_start= true;
 });
 
-// Question Functions
+// QUESTIONS FUNCTION: display questions and multiple-choice answers
 
 function setQuizQuestions() {
         questionHeading.textContent = questionsArray[i].question;
@@ -94,9 +96,9 @@ function setQuizQuestions() {
 
 // When user answers a question: then user is presented with another question
 
-// Store user answer choices. Clear elements/update score count.
+// Store user answer choices. Clear elements and update score count.
 
-// Change - next question
+// Change to next question
 answerChoiceA.addEventListener('click', function(event) {
         event.stopPropagation();
         correctAnswer= questionsArray[i].correctAnswer;
@@ -222,8 +224,8 @@ answerChoiceD.addEventListener('click', function(event) {
     };
 });
 
-        //end
-        unction end_quiz(){
+        //end quiz
+        function end_quiz(){
             document.getElementById("game_over").style.display= "block";
             document.getElementById("quizContainer").style.display="none";
             document.getElementById("countdownTimer").style.display= "none";
@@ -278,4 +280,3 @@ answerChoiceD.addEventListener('click', function(event) {
         i=0;
         score=0;
         }
-        // finish
